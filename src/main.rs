1adae7d16f0ac094d6deba9f27d2d4b1ab03457d
naari3 <naari.named@gmail.com>
2022-02-20 16:25:00 +0900

    Refactored; move each method to manga_cross

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-use manga_cross::{Comic, Episode};
-use reqwest::header::CONTENT_TYPE;
-use rss::{
-    Channel, ChannelBuilder, EnclosureBuilder, GuidBuilder, ImageBuilder, Item, ItemBuilder,
-};
 use tokio::fs::File;
 use tokio::io::AsyncWriteExt;
 
@@ -13,76 +8,6 @@ use crate::manga_cross::MangaCrossComic;
 mod manga_cross;
 
 const BOKUYABA_JSON_URL: &str = "https://mangacross.jp/api/comics/yabai.json?type=public";
-const MANGACROSS_HOST: &str = "https://mangacross.jp";
-
-async fn make_item(ep: &Episode, comic: &Comic) -> eyre::Result<Item> {
-    info!("Make ep {} start", ep.sort_volume);
-    let mut item = ItemBuilder::default();
-    let guid = GuidBuilder::default()
-        .value(format!("{}{}", MANGACROSS_HOST, ep.page_url))
-        .permalink(true)
-        .build();
-    info!("Make ep {} download image start", ep.sort_volume);
-    let image_res = reqwest::get(ep.list_image_double_url.as_str()).await?;
-    info!("Make ep {} download image done", ep.sort_volume);
-    let mime_type = match image_res.headers().get(CONTENT_TYPE) {
-        Some(content_type) => content_type.to_str()?,
-        None => "",
-    };
-    let length = match image_res.content_length() {
-        Some(n) => n.to_string(),
-        None => "".to_string(),
-    };
-    let enclosure = EnclosureBuilder::default()
-        .url(ep.list_image_double_url.clone())
-        .mime_type(mime_type)
-        .length(length)
-        .build();
-    info!("Make ep {} done", ep.sort_volume);
-
-    Ok(item
-        .title(format!("{} | {}", ep.volume, ep.title))
-        .link(format!("{}{}", MANGACROSS_HOST, ep.page_url))
-        .guid(guid)
-        .pub_date(ep.publish_start.clone())
-        .author(comic.author.clone())
-        .enclosure(enclosure)
-        .build())
-}
-
-async fn make_channel(comic: &Comic) -> eyre::Result<Channel> {
-    info!("Make channel {} start", comic.title);
-    let mut channel = ChannelBuilder::default()
-        .title(comic.title.clone())
-        .link(format!(
-            "{}/comics/{}/",
-            MANGACROSS_HOST,
-            comic.dir_name.clone()
-        ))
-        .description(comic.caption_for_search.clone())
-        .image(
-            ImageBuilder::default()
-                .url(comic.image_url.clone())
-                .link(comic.image_url.clone())
-                .title(format!("{} {}", &comic.title, &comic.author))
-                .build(),
-        )
-        .pub_date(comic.latest_episode_publish_start.clone())
-        .last_build_date(comic.latest_episode_publish_start.clone())
-        .build();
-
-    let items: Vec<Item> = futures::future::try_join_all(
-        comic
-            .episodes
-            .iter()
-            .filter(|ep| ep.status == "public")
-            .map(|ep| make_item(ep, &comic)),
-    )
-    .await?;
-    channel.set_items(items);
-    info!("Make channel {} done", comic.title);
-    Ok(channel)
-}
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
@@ -94,16 +19,14 @@ async fn main() -> eyre::Result<()> {
     info!("Get bokuyaba json");
 
     let bokuyaba: MangaCrossComic = serde_json::from_str(body.as_str())?;
-    let bokuyaba = bokuyaba.comic;
 
     info!("Create feed start");
-    let channel = make_channel(&bokuyaba).await?;
+    let channel = bokuyaba.to_channel().await?;
     let feed = channel.to_string();
     info!("Create feed done");
 
-    info!("Create feed.xml");
+    info!("Write to feed.xml");
     let mut file = File::create("feed.xml").await?;
-    info!("Write feed.xml");
     file.write_all(feed.as_bytes()).await?;
     info!("Done");
 
--- src/manga_cross.rs
+++ src/manga_cross.rs
@@ -1,8 +1,54 @@
+use log::info;
+use reqwest::header::CONTENT_TYPE;
+use rss::{
+    Channel, ChannelBuilder, EnclosureBuilder, GuidBuilder, ImageBuilder, Item, ItemBuilder,
+};
 use serde::{Deserialize, Serialize};
+
+const MANGACROSS_HOST: &str = "https://mangacross.jp";
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct MangaCrossComic {
     pub comic: Comic,
+}
+
+impl MangaCrossComic {
+    pub async fn to_channel(&self) -> eyre::Result<Channel> {
+        let comic = &self.comic;
+
+        info!("to_channel {} start", comic.title);
+        let mut channel = ChannelBuilder::default()
+            .title(comic.title.clone())
+            .link(format!(
+                "{}/comics/{}/",
+                MANGACROSS_HOST,
+                comic.dir_name.clone()
+            ))
+            .description(comic.caption_for_search.clone())
+            .image(
+                ImageBuilder::default()
+                    .url(comic.image_url.clone())
+                    .link(comic.image_url.clone())
+                    .title(format!("{} {}", &comic.title, &comic.author))
+                    .build(),
+            )
+            .pub_date(comic.latest_episode_publish_start.clone())
+            .last_build_date(comic.latest_episode_publish_start.clone())
+            .build();
+
+        let items: Vec<Item> = futures::future::try_join_all(
+            comic
+                .episodes
+                .iter()
+                .filter(|ep| ep.status == "public")
+                .map(|ep| ep.to_item(&comic)),
+        )
+        .await?;
+        channel.set_items(items);
+
+        info!("to_channel {} done", comic.title);
+        Ok(channel)
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
@@ -85,6 +131,43 @@ pub struct Episode {
     pub episode_next_date: Option<String>,
     pub next_date_customize_text: Option<String>,
     pub is_unlimited_comic: bool,
+}
+
+impl Episode {
+    pub async fn to_item(&self, comic: &Comic) -> eyre::Result<Item> {
+        info!("to_item {} start", self.sort_volume);
+        let mut item = ItemBuilder::default();
+        let guid = GuidBuilder::default()
+            .value(format!("{}{}", MANGACROSS_HOST, self.page_url))
+            .permalink(true)
+            .build();
+        info!("to_item {} download image start", self.sort_volume);
+        let image_res = reqwest::get(self.list_image_double_url.as_str()).await?;
+        info!("to_item {} download image done", self.sort_volume);
+        let mime_type = match image_res.headers().get(CONTENT_TYPE) {
+            Some(content_type) => content_type.to_str()?,
+            None => "",
+        };
+        let length = match image_res.content_length() {
+            Some(n) => n.to_string(),
+            None => "".to_string(),
+        };
+        let enclosure = EnclosureBuilder::default()
+            .url(self.list_image_double_url.clone())
+            .mime_type(mime_type)
+            .length(length)
+            .build();
+        info!("to_item {} done", self.sort_volume);
+
+        Ok(item
+            .title(format!("{} | {}", self.volume, self.title))
+            .link(format!("{}{}", MANGACROSS_HOST, self.page_url))
+            .guid(guid)
+            .pub_date(self.publish_start.clone())
+            .author(comic.author.clone())
+            .enclosure(enclosure)
+            .build())
+    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
